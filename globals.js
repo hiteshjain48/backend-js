82ddb4442cfd245c6c462d870b5449e79ca693e9
hiteshjain48 <hiteshjainhd@gmail.com>
2023-05-03 19:11:09 +0530

    added global variables file

---
+++ globals.js
@@ -0,0 +1,10 @@
+//Globals --- NO WINDOW !!!!
+
+// __dirname    - path to current directory
+// __filename   - file name
+// require      - function to use module (Common JS)  
+// module       - info about current module (file)
+// process      - info about env where the program is being executed
+
+console.log(__dirname)
+console.log(process)
